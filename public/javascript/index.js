f1dbf663a8147d2193876363fef3afe88507f1ae
Miguel Curbelo <mcurbeloj@gmail.com>
2022-11-25 20:03:23 +0100

    lab completed

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,79 @@
+const router = require("express").Router();
+
+const APIHandler = require('./APIHandler');
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    router.get('/characters', (req, res) => {
+      charactersAPI
+      .getAllCharacters()
+      .then((response) => {
+        console.log(response.data)
+        res.json(response.data)
+        res.render('/characters', { characters: response.data })
+      })
+      .catch(error => console.log(error));
+    })
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    router.get('/characters/:id', (req, res) => {
+      const characterId = req.params.id;
+      charactersAPI
+      .getSingleCharacter(characterId)
+      .then((response) => {
+        console.log(response.data)
+        res.json(response.data)
+        res.render(`/characters/${characterId}`, { oneCharacter: response.data })
+      })
+      .catch(error => console.log(error));
+    })
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    router.delete('/characters/:id', (req, res) => {
+      const characterId = req.params.id;
+      charactersAPI
+      .deleteCharacter(characterId)
+      .then((response) => {
+        console.log(response.data)
+        res.json(response.data)
+        res.render('/characters', { characters: response.data }, { deletedCharacter: true })
+      })
+      .catch(error => console.log(error))
+    })
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    router.put('/characters/:id', (req, res) => {
+      const characterId = req.params.id;
+      const characterInfo = req.body;
+      charactersAPI
+      .editCharacter(characterId, characterInfo)
+      .then((response) => {
+        console.log(response.data)
+        res.json(response.data)
+        res.render(`/characters/${charachterId}`, { oneCharacter: response.data })
+      })
+      .catch(error => console.log(error))
+    })
+    event.preventDefault();
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    router.post('/characters', (req, res) => {
+      const characterInfo = req.body;
+      charactersAPI
+      .createCharacter(characterInfo)
+      .then((response) => {
+        const newCharacterId = response.data.id;
+        console.log(response.data)
+        res.json(response.data)
+        res.render(`/characters/${newCharacterId}`, { oneCharacter: response.data })
+      })
+      .catch(error => console.log(error))
+    })
+    event.preventDefault();
   });
 });
--- services/api.services.js
+++ services/api.services.js
@@ -3,27 +3,29 @@ const axios = require('axios');
 class ApiHandler {
     constructor () {
         this.api = axios.create({
-            baseURL: 'http://localhost:8000/characters'
+            baseURL: 'http://localhost:8000/'
         })
     }
 
     getAllCharacters = () => {
-        return this.api.get('/');
+        return this.api.get('/characters');
     };
 
     getSingleCharacter = (characterId) => {
-        return this.api.get(`/${characterId}`);
+        return this.api.get(`/characters/${characterId}`);
     }
 
     createCharacter = (characterInfo) => {
-        return this.api.post('/', characterInfo)
+        return this.api.post('/characters', characterInfo)
     }
 
     deleteCharacter = (characterId) => {
-        return this.api.delete(`/${characterId}`)
+        return this.api.delete(`/characters/${characterId}`)
     }
 
     editCharacter = (characterId, characterInfo) => {
-        return this.api.put(`/${characterId}`, characterInfo)
+        return this.api.put(`/characters/${characterId}`, characterInfo)
     }
 }
+
+module.exports = ApiHandler;
